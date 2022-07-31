cf71f2879e11f83e975727ac85a04d25abb8ca7a
ftijan <41797259+ftijan@users.noreply.github.com>
2022-08-01 00:47:09 +0100

    adding solution files (#1)
    * basic styles and elements
    * add animation styles
    * change animation logic, add script base
    * move flip logic to function
    * more timer digits and styles
    * logic done
    * updated docs
    * title update

---
+++ scripts.js
@@ -0,0 +1,60 @@
+const flipCard = document.querySelector(".flip-card")
+
+// new 24 hour timer
+const countToDate = new Date().setHours(new Date().getHours() + 24)
+
+let previousTimeBetweenDates
+setInterval(() => {
+    const currentDate = new Date()
+    const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000)
+    
+    flipAllCards(timeBetweenDates)    
+
+    previousTimeBetweenDates = timeBetweenDates
+}, 250)
+
+function flipAllCards(time) {
+    const seconds = time % 60
+    const minutes = Math.floor(time / 60 % 60)
+    const hours = Math.floor(time / 3600)
+    
+    flip(document.querySelector("#hours-tens"), Math.floor(hours / 10))
+    flip(document.querySelector("#hours-ones"), hours % 10)
+    flip(document.querySelector("#minutes-tens"), Math.floor(minutes / 10))
+    flip(document.querySelector("#minutes-ones"), minutes % 10)
+    flip(document.querySelector("#seconds-tens"), Math.floor(seconds / 10))
+    flip(document.querySelector("#seconds-ones"), seconds % 10)
+}
+
+function flip(flipCard, newNumber) {
+    const topHalf = flipCard.querySelector(".top")
+    const startNumber = parseInt(topHalf.textContent)
+
+    if (newNumber === startNumber) return
+
+    const bottomHalf = flipCard.querySelector(".bottom")
+    const topFlip = document.createElement("div")
+    topFlip.classList.add("top-flip")
+    const bottomFlip = document.createElement("div")
+    bottomFlip.classList.add("bottom-flip")
+
+    topHalf.textContent = startNumber
+    bottomHalf.textContent = startNumber
+    topFlip.textContent = startNumber
+    bottomFlip.textContent = newNumber
+
+    topFlip.addEventListener("animationstart", e => {
+        topHalf.textContent = newNumber
+    })
+
+    topFlip.addEventListener("animationend", e => {
+        topFlip.remove()
+    })
+
+    bottomFlip.addEventListener("animationend", e => {
+        bottomHalf.textContent = newNumber
+        bottomFlip.remove()
+    })
+
+    flipCard.append(topFlip, bottomFlip)
+}
